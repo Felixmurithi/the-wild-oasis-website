69373d53c748d66ea885efad06f5b24efff6209f
Felixmurithi <murithifelixgims@gmail.com>
2024-06-13 17:15:29 +0300

    file name casing error 2

--- app/account/reservations/edit/[bookingId]/page.js
+++ app/account/reservations/edit/[bookingId]/page.js
@@ -1,4 +1,4 @@
-import UpdateReservationForm from "@/app/_components/UpdateReservationFormpdateReservationForm";
+import UpdateReservationForm from "@/app/_components/UpdateReservationFormp";
 import { getBooking, getSettings } from "@/app/_lib/data-service";
 
 export default async function Page({ params }) {
